6dc6a28a7e437b77dbdd741fbee54307e39638b8
Gintars Briedis <gintars.briedis@gmail.com>
2021-07-14 10:38:35 +0300

    ModifiedProfile Class

--- Plugin1/Clases/Profile.cs
+++ Plugin1/Clases/Profile.cs
@@ -8,10 +8,40 @@ namespace Plugin1.Clases
 {
     class Profile
     {
+        //Ctor
         public Profile()
         {
-
+            
         }
+        //Atributes
+        public int Number { get; set; }
+        public string ProfileDescription { get; set; }
+        //Private Atributes
+        private string _profileName;
+
+        //Atribute Getters/Setters
+        public string ProfileName
+        {
+            get { return _profileName; }
+            set 
+            { 
+                var profilestringparts = ProfileDescription.Split(' ').ToList();
+                if (profilestringparts[0]=="QRO")
+                {
+                    _profileName = "SHS" + profilestringparts[1];
+                }
+                if (profilestringparts[0] == "RRO")
+                {
+                    _profileName = "RHS" + profilestringparts[1];
+                }
+                else
+                {
+                    _profileName = profilestringparts[0] + profilestringparts[1];
+                }
+            }
+        }
+        //Methods
+
 
     }
 }
